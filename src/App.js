86db5130e4f8166ec97f0e1ba8a42baac0de7fa5
Breyanna Vass <breyannavass@gmail.com>
2019-10-17 17:31:33 -0400

    Adding form component and adjusting app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 
+import { Grid, Typography, Paper, Divider } from "@material-ui/core";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to React Game</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Grid
+        container
+        style={{ height: "100vh" }}
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={3}>
+          <Paper style={{ padding: "50px" }} elevation={6}>
+            <Typography align="center" variant="h2" gutterBottom>
+              HOT or COLD
+            </Typography>
+            <Divider style={{ margin: "20px 0" }} />
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { TextField, Button } from "@material-ui/core";
+
+const Form = () => {
+  return (
+    <form>
+      <TextField style={{ paddingBottom: "20px" }} />
+    </form>
+  );
+};
+
+export default Form;
