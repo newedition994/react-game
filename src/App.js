7ce051beb63f30db0f9c55011abf01fdc1630fa9
Breyanna Vass <breyannavass@gmail.com>
2019-10-17 19:30:43 -0400

    updating all the functional components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import React from "react";
 import { Grid, Typography, Paper, Divider } from "@material-ui/core";
 
 import Form from "./components/Form";
+import Progess from "./components/Progress";
+import { generateRandomNumber } from "./util";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    generatedNumber: generateRandomNumber(),
+    guess: undefined,
+    allGuesses: [],
+    attempt: 0
+  };
+
   updateAppState = guess => {
     console.log(guess);
+
+    const absDiff = Math.abs(guess - this.state.generatedNumber);
+
+    this.setState(prevState => ({
+      guess,
+      allGuesses: [...prevState.allGuesses, { guess }],
+      attempt: prevState.attempt + 1
+    }));
   };
 
   render() {
+    const { allGuesses, attempt } = this.state;
+
+    const guessList = allGuesses.map((item, index) => (
+      <li key={index}>
+        <span>{item.guess}</span>
+      </li>
+    ));
+
     return (
       <Grid
         container
@@ -25,6 +50,7 @@ class App extends React.Component {
             </Typography>
             <Divider style={{ margin: "20px 0" }} />
             <Form returnGuessToApp={guess => this.updateAppState(guess)} />
+            <Progess attempt={attempt} guessList={guessList}></Progess>
           </Paper>
         </Grid>
       </Grid>
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import { Typography } from "@material-ui/core";
+import "./Progress.css";
 
-const Progress = () => {
+const Progress = ({ attempt, guestList }) => {
   return (
     <div>
-      <Typography className="progressBar" variant="h2">
-        Guess # {attempt}{" "}
-      </Typography>
+      <Typography variant="h2">Guess # {attempt}</Typography>
       <ul className="progressBar_history">{guestList}</ul>
     </div>
   );
