57a35fe8457176b7e1b110c2e54d72c0335d6443
Breyanna Vass <breyannavass@gmail.com>
2019-10-17 19:37:10 -0400

    adjusting styles

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -6,7 +6,9 @@ import "./Progress.css";
 const Progress = ({ attempt, guestList }) => {
   return (
     <div>
-      <Typography variant="h2">Guess # {attempt}</Typography>
+      <Typography style={{ marginTop: "50px" }} variant="h2">
+        Guess # {attempt}
+      </Typography>
       <ul className="progressBar_history">{guestList}</ul>
     </div>
   );
